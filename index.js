19496a615ac4984d8bc0b50d0ed9855ae9faa033
Florent Menus <florent.mns@gmail.com>
2022-06-22 11:28:10 +0200

    finished 4

---
+++ index.js
@@ -0,0 +1,47 @@
+// 1 - Fruits
+
+// const fruits = ["mango","lemon","blueberry"]
+
+// console.log (fruits);
+// console.table(fruits);
+
+
+// 2 - Access
+
+// const ingredients = ["eggs","milk","butter"]
+
+// console.log(ingredients[1])
+// console.log(ingredients .indexOf("butter"))
+
+
+// 3- Add and Removes
+
+// const objects = ["pen","book","lamp"]
+
+// objects.unshift("chair")
+// console.log(objects)
+
+// objects.pop()
+// console.log(objects)
+
+// objects.push("laptop")
+// console.log(objects)
+
+// objects.shift()
+// console.log(objects)
+
+// 4- Order
+
+// const numbers = [4,10,8,12,6];
+// numbers.reverse();
+
+// console.log(numbers)
+
+
+// 5- Boucle
+
+let total = 0;
+const limit = 10;
+
+for (i=total, limit, total+=i){
+}
